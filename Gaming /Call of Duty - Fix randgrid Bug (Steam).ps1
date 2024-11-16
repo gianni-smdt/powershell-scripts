576c1b868638c1265982d86f59d70816f12cd962
Gianni <giannischmidt2001@gmail.com>
2024-11-16 12:28:50 +0100

    Create Call of Duty - Fix randgrid Bug (Steam).ps1

---
+++ Gaming /Call of Duty - Fix randgrid Bug (Steam).ps1
@@ -0,0 +1,3 @@
+#Start Windows PowerShell as administrator and insert the following command line
+                #Replace C:\ and D:\ with your specific drive letters
+Move-Item -Path "C:\SteamLibrary\steamapps\common\Call of Duty HQ\randgrid.sys" -Destination "D:\SteamLibrary\steamapps\common\Call of Duty HQ\randgrid.sys"  
