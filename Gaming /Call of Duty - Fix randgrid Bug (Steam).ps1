3b776c229dbec6bc02e7c515000da7657b41ec81
Gianni <giannischmidt2001@gmail.com>
2024-11-16 12:34:43 +0100

    Update Call of Duty - Fix randgrid Bug (Steam).ps1

--- Gaming /Call of Duty - Fix randgrid Bug (Steam).ps1
+++ Gaming /Call of Duty - Fix randgrid Bug (Steam).ps1
@@ -1,3 +1,10 @@
-#Start Windows PowerShell as administrator and insert the following command line
-                #Replace C:\ and D:\ with your specific drive letters
+<#
+.DESCRIPTION
+    This script/command fixes the error "Failed to open \??\D:\SteamLibrary\steamapps\common\Call of Duty HQ\randgrid.sys".
+
+.NOTES
+    You have to start Windows PowerShell as administrator
+#>
+
+#Replace C:\ and D:\ with your specific drive letters
 Move-Item -Path "C:\SteamLibrary\steamapps\common\Call of Duty HQ\randgrid.sys" -Destination "D:\SteamLibrary\steamapps\common\Call of Duty HQ\randgrid.sys"  
